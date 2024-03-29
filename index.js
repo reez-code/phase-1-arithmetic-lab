a3da6a6f573b76657ae3f1a40a8b76949d438052
Phil <phil.github@gmail.com>
2024-03-29 12:16:39 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 15;
+const num4 = 11;
+const mod = num3 % num4;
+
+const max = Math.max(3, 4, 5, 6, 7, 8, 9, 10, 20);
